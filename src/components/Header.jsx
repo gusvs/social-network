d5db9bdb5a1cb46726b215ea42218e62de29c9af
gusvs <bigvital@mail.ru>
2021-03-02 14:38:34 +0500

    12. Splitting on the components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { React } from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://img2.freepng.ru/20180426/zwq/kisspng-hewlett-packard-logo-hewlett-packard-enterprise-hp-laptop-5ae168d447ca66.8372165515247218762941.jpg"></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import { React } from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://avatars.mds.yandex.net/get-zen_doc/48747/pub_5a29011e9d5cb34737864543_5a29013ce86a9e41b4f4b9f3/scale_1200"></img>
+            </div>
+            <div>
+                ava + description
+        </div>
+            <div>
+                My Posts
+          <div>
+                    New post
+          </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                    <div>Post 3</div>
+                    <div>Post 4</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;

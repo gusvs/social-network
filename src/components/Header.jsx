6e87e6ec447e817dc569c7c11d9f84ed4450660d
gusvs <bigvital@mail.ru>
2021-03-03 11:17:07 +0500

    Added css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import { React } from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={s.header}>
             <img src="https://img2.freepng.ru/20180426/zwq/kisspng-hewlett-packard-logo-hewlett-packard-enterprise-hp-laptop-5ae168d447ca66.8372165515247218762941.jpg"></img>
         </header>
     );

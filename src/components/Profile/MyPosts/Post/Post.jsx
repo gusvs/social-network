3a5fe60c27b95623d02d071153b16443353e07d9
gusvs <bigvital@mail.ru>
2021-03-03 12:11:20 +0500

    15. Changed README.md and Folders structure

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,16 @@
+import { React } from "react";
+import s from "./Post.module.css";
+
+const Post = () => {
+	return (
+		<div className={s.item}>
+			<img src="https://i.pinimg.com/736x/0a/dd/87/0add874e1ea0676c4365b2dd7ddd32e3--james-cameron-james-darcy.jpg"></img>
+			Post 1
+            <div>
+                <span>Like</span>
+            </div>
+		</div>
+	);
+};
+
+export default Post;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import { React } from "react";
 import s from "./Profile.module.css";
+import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
@@ -8,16 +9,7 @@ const Profile = () => {
                 <img src="https://avatars.mds.yandex.net/get-zen_doc/48747/pub_5a29011e9d5cb34737864543_5a29013ce86a9e41b4f4b9f3/scale_1200"></img>
             </div>
             <div>ava + description</div>
-            <div>
-                My Posts
-				<div>New post</div>
-                <div>
-                    <div className={s.item}>Post 1</div>
-                    <div className={s.item}>Post 2</div>
-                    <div>Post 3</div>
-                    <div>Post 4</div>
-                </div>
-            </div>
+            <MyPosts />
         </div>
     );
 };

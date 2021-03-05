0d5fd463acca249599d63fdb5164cf093a638eff
gusvs <bigvital@mail.ru>
2021-03-05 12:22:33 +0500

    Added Dialogs Layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,21 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>Dima</div>
+                <div className={`${s.dialog} ${s.active}`}>Stas</div>
+                <div className={s.dialog}>Egor</div>
+                <div className={s.dialog}>Ivan</div>
+                <div className={s.dialog}>Oleg</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello, how are you?</div>
+                <div className={s.message}>Hello, how are you?</div>
+                <div className={s.message}>Hello, how are you?</div>
+                <div className={s.message}>Hello, how are you?</div>
+                <div className={s.message}>Hello, how are you?</div>
+            </div>
         </div>
     );
 }

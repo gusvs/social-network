4d386f306270cef9732b251466bc29cd571edb0f
gusvs <bigvital@mail.ru>
2021-04-14 17:47:32 +0500

    35. Callback, subscribe, observer

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react';
 import state from './redux/state';
-// import { addPost } from './redux/state';
-// import { BrowserRouter } from 'react-router-dom';
-import { rerenderEntireTree } from './render';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { addPost, updateNewPostText, subscribe } from './redux/state';
+import { BrowserRouter } from 'react-router-dom';
 
-// let rerenderEntireTree = () => {
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <BrowserRouter>
-//         <App state={state} addPost={addPost} />
-//       </BrowserRouter>
-//     </React.StrictMode>,
-//     document.getElementById('root')
-//   );
-// }
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App state={state} addPost={addPost}
+          updateNewPostText={updateNewPostText} />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from '../render';
+let rerenderEntireTree = () => {
+    console.log('state changed');
+}
 
 let state = {
     profilePage: {
@@ -28,7 +30,7 @@ let state = {
     }
 }
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -40,9 +42,13 @@ export let addPost = () => {
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 }
 
 export default state

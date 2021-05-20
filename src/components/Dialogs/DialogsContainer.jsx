37ecdc3675bb1cb2e58670ab7f896cbf90f86d2f
gusvs <bigvital@mail.ru>
2021-05-20 20:36:48 +0500

    45. react-redux, connect, mapStateToProps, mapDispatchToProps

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
 import {updateNewMessageBodyCreator, sendMessageCreator} from '../../redux/dialogsReducer';
 import Dialogs from './Dialogs';
-import StoreContext from "../../StoreContext";
+import { connect } from 'react-redux';
 
-const DialogsContainer = () => {
-    return (
-        <StoreContext.Consumer>
-            {
-                (store) => {
-                    let state = store.getState().dialogsPage;
-
-                    let onSendMessageClick = () => {
-                        store.dispatch(sendMessageCreator())
-                    }
-                    let onNewMassageChange = (body) => {
-                        store.dispatch(updateNewMessageBodyCreator(body));
-                    }
-                    return <Dialogs
-                        updateNewMessageBody={onNewMassageChange}
-                        sendMessage={onSendMessageClick}
-                        dialogsPage={state}
-                    />
-                }
-            }
-        </StoreContext.Consumer>
-    );
+let mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
 }
+
+let mapDispatchToProps = (dispatch) => {
+ return {
+    updateNewMessageBody: (body) => {
+        dispatch(updateNewMessageBodyCreator(body))
+    },
+    sendMessage: () => {
+        dispatch(sendMessageCreator())
+    }
+ }
+}
+
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
 
 export default DialogsContainer;
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -5,7 +5,7 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <div>
-                <img src="https://i.ibb.co/r4TMgxT/91599a507e7c8ba.jpg"></img>
+                <img className={s.headerImg}src="https://i.ibb.co/r4TMgxT/91599a507e7c8ba.jpg"></img>
             </div>
             <div className={s.descriptionBlock}>ava + description</div>
         </div>

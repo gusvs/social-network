d328dc7e30833a3b9611bb16232c4e3ee982a5a9
gusvs <bigvital@mail.ru>
2021-08-12 13:37:19 +0500

    47. stateCopy in ProfileReducer

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {updateNewMessageBodyCreator, sendMessageCreator} from '../../redux/dialogsReducer';
+import { updateNewMessageBodyCreator, sendMessageCreator } from '../../redux/dialogsReducer';
 import Dialogs from './Dialogs';
 import { connect } from 'react-redux';
 
@@ -10,14 +10,14 @@ let mapStateToProps = (state) => {
 }
 
 let mapDispatchToProps = (dispatch) => {
- return {
-    updateNewMessageBody: (body) => {
-        dispatch(updateNewMessageBodyCreator(body))
-    },
-    sendMessage: () => {
-        dispatch(sendMessageCreator())
+    return {
+        updateNewMessageBody: (body) => {
+            dispatch(updateNewMessageBodyCreator(body))
+        },
+        sendMessage: () => {
+            dispatch(sendMessageCreator())
+        }
     }
- }
 }
 
 const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
--- src/index.js
+++ src/index.js
@@ -7,24 +7,17 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-let rerenderEntireTree = (state) => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={store}>
-                    <App />
-                </Provider>
-            </BrowserRouter>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
-rerenderEntireTree();
-
-store.subscribe(() => {
-    rerenderEntireTree();
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

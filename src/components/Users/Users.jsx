32ec3fb8be40875e49c7c223bb53a7d611fc7328
gusvs <bigvital@mail.ru>
2021-11-14 23:07:20 +0500

    54. Life cycle methods. Refactor Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,8 +4,7 @@ import s from './Users.module.css'
 import userPhoto from '../../assets/images/user.png'
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props)
+  componentDidMount() {
     axios
       .get('https://social-network.samuraijs.com/api/1.0/users')
       .then((response) => {

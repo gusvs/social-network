ba6522dd72ec9c41df483aa1496397be9417fc21
gusvs <bigvital@mail.ru>
2021-12-14 22:57:11 +0500

    70. Function compose()

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { getUserProfile } from '../../redux/profileReducer'
 import { withRouter } from 'react-router-dom'
 import { withAuthRedirect } from '../../hoc/withAuthRedirect'
+import { compose } from 'redux'
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
@@ -16,14 +17,12 @@ class ProfileContainer extends React.Component {
   }
 }
 
-let AuthRedirectComponent = withAuthRedirect(ProfileContainer)
-
 let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 })
 
-let WithUrlDataContainer = withRouter(AuthRedirectComponent)
-
-export default connect(mapStateToProps, { getUserProfile })(
-  WithUrlDataContainer
-)
+export default compose(
+  connect(mapStateToProps, { getUserProfile }),
+  withRouter,
+  withAuthRedirect
+)(ProfileContainer)

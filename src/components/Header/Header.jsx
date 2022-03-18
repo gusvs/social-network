bfe22fbc30e6e8bf274449edda58691d134fb5da
gusvs <bigvital@mail.ru>
2022-03-18 12:00:55 +0500

    78. Login, Logout API

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,9 @@ const Header = (props) => {
     <header className={s.header}>
       <img src="https://img2.freepng.ru/20180426/zwq/kisspng-hewlett-packard-logo-hewlett-packard-enterprise-hp-laptop-5ae168d447ca66.8372165515247218762941.jpg"></img>
       <div className={s.loginBlock}>
-        {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+        {props.isAuth
+          ? <div>{props.login} - <button onClick={props.logout}>Log out</button></div>
+          : <NavLink to={'/login'}>Login</NavLink>}
       </div>
     </header>
   )

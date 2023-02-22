ab732d516576da19c0910a91d2e4d84ba581811e
gusvs <bigvital@mail.ru>
2023-02-22 23:26:24 +0500

    83. Install reselect

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,6 +1,12 @@
-export const getUsers = (state) => {
+import {createSelector} from "reselect";
+
+const getUsersSelector = (state) => {
     return state.usersPage.users
 }
+
+export const getUsers = createSelector(getUsersSelector, (users) => {
+    return users.filter(u => true)
+})
 export const getPageSize = (state) => {
     return state.usersPage.pageSize
 }
@@ -16,12 +22,3 @@ export const getIsFetching = (state) => {
 export const getFollowingInProgress = (state) => {
     return state.usersPage.followingInProgress
 }
-
-
-
-// users: state.usersPage.users,
-//     pageSize: state.usersPage.pageSize,
-//     totalUsersCount: state.usersPage.totalUsersCount,
-//     currentPage: state.usersPage.currentPage,
-//     isFetching: state.usersPage.isFetching,
-//     followingInProgress: state.usersPage.followingInProgress,
